15475adbdedd0f8f4275e18c1e9cc8bc9af6ae37
Gurpreet Singh <gur.singh2022@gmail.com>
2023-03-26 22:06:39 +0100

    setup eslint, prettier and airbnb styleguide

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,47 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'airbnb-base',
+		'airbnb-typescript/base',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'plugin:prettier/recommended'
+	],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	overrides: [
+		{
+			files: ['**/*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	parserOptions: {
+		project: './tsconfig.eslint.json',
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	settings: {
+		// 'import/parsers': {
+		// 	'@typescript-eslint/parser': ['.cjs', '.js', '.ts', '.svelte']
+		// },
+		// 'import/resolver': {
+		// 	typescript: {
+		// 		alwaysTryTypes: true
+		// 	}
+		// }
+	},
+	rules: {
+		'import/no-extraneous-dependencies': 0
 	}
 };
