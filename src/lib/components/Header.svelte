ebc42ee4368a4429980ffdca17e1be11ab2b877e
Gurpreet Singh <gur.singh2022@gmail.com>
2023-03-31 23:05:47 +0100

    Setup Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar>
+	<svelte:fragment slot="lead">
+		<i class="fa-solid fa-khanda" />
+	</svelte:fragment>
+	Panjabi Kosh
+	<svelte:fragment slot="trail">
+		<button type="button" class="btn variant-filled">Button</button>
+	</svelte:fragment>
+</AppBar>
