6d2b32c9dcc2cd5efb775e329c05e4931a6c91e8
Gurpreet Singh <gur.singh2022@gmail.com>
2023-04-01 23:30:32 +0100

    Layout update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	import Header from '../lib/components/Header.svelte';
+
 	// Your selected Skeleton theme:
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-
 	// This contains the bulk of Skeletons required styles:
 	import '@skeletonlabs/skeleton/styles/all.css';
-
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<Header />
+	</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
