1889697b4eef9dea810c71395b5e4f5129c7c080
Gurpreet Singh <gur.singh2022@gmail.com>
2023-04-02 22:11:10 +0100

    Add template word page

---
+++ src/routes/[word]/+page.svelte
@@ -0,0 +1,39 @@
+<div class="container mx-auto py-10">
+	<div class="flex gap-5">
+		<div class="basis-3/4 text-white">
+			<div class="bg-gray-600 p-8">
+				<div class="flex items-center gap-16 border-b border-white">
+					<div class="text-6xl">Lesson</div>
+					<div class="abbrev">n</div>
+				</div>
+				<div class="border-b border-white">
+					<div class="desc">Grammar</div>
+					<ol class="ml-4 list-decimal pl-4">
+						<li>Lorem, ipsum.</li>
+						<li>Lorem ipsum dolor sit amet.</li>
+						<li>Lorem, ipsum dolor.</li>
+					</ol>
+				</div>
+				<div class="">
+					<div class="desc-en">noun</div>
+					<ol class="ml-4 list-decimal pl-4">
+						<li>lesson</li>
+					</ol>
+				</div>
+			</div>
+
+			<div class="mt-5 bg-gray-600 p-8">
+				<div class="">
+					<div class="desc-en">noun</div>
+					<ol class="ml-4 list-decimal pl-4">
+						<li>lesson</li>
+					</ol>
+				</div>
+			</div>
+		</div>
+
+		<aside class="basis-1/4">
+			<img src="https://picsum.photos/1200" alt="" />
+		</aside>
+	</div>
+</div>
